608c34e9913a022fadbc9a217e442bf6b2c9f180
Matheus - Aula 1 <matheusguedescosta@gmail.com>
2023-04-08 04:37:08 -0300

    primeira pg finalizada

---
+++ assets/js/digitando-nome.js
@@ -0,0 +1,20 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+  var typed = new Typed(".texto-escrevendo", {
+    strings: ["Matheus Guedes", "Dev Front End"],
+
+    backDelay: 1800,
+    typeSpeed: 90,
+    backSpeed: 80,
+    loop: true,
+    startDelay: 3500,
+    onComplete: function (self) {
+      self.startDelay = 0;
+    },
+  });
+});
+
+
+setTimeout(function () {
+  $('.ocultar').fadeIn();
+}, 3500);
