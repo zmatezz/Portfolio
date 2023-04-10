db16122961aa779573a7636b4a339288b8973009
Matheus - Aula 1 <matheusguedescosta@gmail.com>
2023-04-10 01:28:53 -0300

    segunda pg finalizada, teste scroll reveal

---
+++ assets/js/scroll-reveal.js
@@ -0,0 +1,9 @@
+window.sr = ScrollReveal({ reset: true })
+sr.reveal(".sobre-mim", {
+  origin: "top", //Para ele surgir de cima para baixo.
+  distance: "50px", // A distancia do origin para a posição original do elemento.
+  duration: 2000, // A duração da animação em ms
+  reset: true // Ao sair do campo de tela, a animação não acontecerá novamente caso eu passe por ela novamente.
+});
+
+
