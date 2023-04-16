fb7cee2ff5c71fe112e1072a06e279ec90157a40
Matheus - Aula 1 <matheusguedescosta@gmail.com>
2023-04-16 20:08:34 -0300

    prod2 tecnologias

--- assets/js/habilidades.js
+++ assets/js/habilidades.js
@@ -1,26 +1,27 @@
 const imagens = document.querySelectorAll('.tecnologias');
 const divTexto = document.querySelector('.div-texto');
 
-const textos = {
-    'imagem-1': 'Texto 1',
-    'imagem-2': 'Texto 2',
-    'imagem-3': 'Texto 3',
-    'imagem-4': 'Texto 4',
-    'imagem-5': 'Texto 5',
-    'imagem-6': 'Texto 6',
-    'imagem-7': 'Texto 7',
-    'imagem-8': 'Texto 8'
+const texto = {
+    'imagem-1': 'HTML<br>Intermediário',
+    'imagem-2': 'CSS<br>Intermédiario',
+    'imagem-3': 'JavaScript<br>Iniciante',
+    'imagem-4': 'GitHub<br>Intermédiario',
+    'imagem-5': 'Vtex<br>Avançado',
+    'imagem-6': 'Sass<br>Intermédiario',
+    'imagem-7': 'React<br>Iniciante',
+    'imagem-8': 'Git<br>Intermédiario',
 };
 
 imagens.forEach(imagem => {
+    const id = imagem.id;
+    const textoImagem = texto[id];
+
     imagem.addEventListener('mouseover', () => {
-        const id = imagem.id;
-        const texto = textos[id];
-        divTexto.textContent = texto;
-        divTexto.style.display = 'block';
+        divTexto.innerHTML = textoImagem;
+        divTexto.style.opacity = '1';
     });
 
     imagem.addEventListener('mouseout', () => {
-        divTexto.style.display = 'none';
+        divTexto.style.opacity = '0';
     });
 });
