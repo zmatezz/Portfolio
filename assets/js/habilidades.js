a3c7419fd30f8a92d8b8a2f554a3e123683f05b5
Matheus - Aula 1 <matheusguedescosta@gmail.com>
2023-04-17 02:08:16 -0300

    habilidades

--- assets/js/habilidades.js
+++ assets/js/habilidades.js
@@ -2,11 +2,11 @@ const imagens = document.querySelectorAll('.tecnologias');
 const divTexto = document.querySelector('.div-texto');
 
 const texto = {
-    'imagem-1': 'Html<br>Intermediário',
+    'imagem-1': 'HTML<br>Intermediário',
     'imagem-2': 'CSS<br>Intermédiario',
     'imagem-3': 'JavaScript<br>Iniciante',
     'imagem-4': 'GitHub<br>Intermédiario',
-    'imagem-5': 'Vtex<br>Intermédiario',
+    'imagem-5': 'VTEX<br>Intermédiario',
     'imagem-6': 'Sass<br>Intermédiario',
     'imagem-7': 'React<br>Iniciante',
     'imagem-8': 'Git<br>Intermédiario',
@@ -31,14 +31,14 @@ const imagens2 = document.querySelectorAll('.tecnologias');
 const divTexto2 = document.querySelector('.div-texto2');
 
 const texto2 = {
-    'imagem-1': 'Html<br>Intermediário',
-    'imagem-2': 'CSS<br>Intermédiario',
-    'imagem-3': 'JavaScript<br>Iniciante',
-    'imagem-4': 'GitHub<br>Intermédiario',
-    'imagem-5': 'Vtex<br>Intermédiario',
-    'imagem-6': 'Sass<br>Intermédiario',
-    'imagem-7': 'React<br>Iniciante',
-    'imagem-8': 'Git<br>Intermédiario',
+    'imagem-1': 'Linguagem de marcação, faz parte da tríade para qualquer aplicação web, com uma boa estrutura podemos criar páginas excelentes.',
+    'imagem-2': 'Linguagem de estilização, faz parte da tríade web, é utilizado para estilizar elementos escritos em uma linguagem de marcação como HTML.',
+    'imagem-3': 'Linguagem de programação, fecha a tríade de tecnologias web, onde as três são utilizados em quase toda web, realizando páginas interativas. ',
+    'imagem-4': 'Plataforma para hospedagem de códigos, possibilitando gerenciamento de projetos, registros e colaborações entre a equipe.',
+    'imagem-5': 'Plataforma SaaS, fornece a estrutura para funcionar uma loja e-commerce e possibilita o gerenciamento de atividades de manutenção em massa.',
+    'imagem-6': 'Linguagem que potencializa o Css, é um pré-processador css que permite a escrita em uma sintaxe mais simples e depois compila esse código. ',
+    'imagem-7': 'React é uma biblioteca JavaScript que permite criar excelentes interfaces para usuários para aplicações web e móveis.',
+    'imagem-8': 'Sistema de controle de versão, bastante utilizada no desenvolvimento de software, gerenciamento do código-fonte ao longo do tempo',
 };
 
 imagens2.forEach(imagems => {
