d64f510ae0ef337f25e123774b395f9869edac7e
Matheus - Aula 1 <matheusguedescosta@gmail.com>
2023-04-17 00:13:32 -0300

    prod3 tecnologias

--- assets/js/habilidades.js
+++ assets/js/habilidades.js
@@ -2,11 +2,11 @@ const imagens = document.querySelectorAll('.tecnologias');
 const divTexto = document.querySelector('.div-texto');
 
 const texto = {
-    'imagem-1': 'HTML<br>Intermediário',
+    'imagem-1': 'Html<br>Intermediário',
     'imagem-2': 'CSS<br>Intermédiario',
     'imagem-3': 'JavaScript<br>Iniciante',
     'imagem-4': 'GitHub<br>Intermédiario',
-    'imagem-5': 'Vtex<br>Avançado',
+    'imagem-5': 'Vtex<br>Intermédiario',
     'imagem-6': 'Sass<br>Intermédiario',
     'imagem-7': 'React<br>Iniciante',
     'imagem-8': 'Git<br>Intermédiario',
@@ -23,5 +23,34 @@ imagens.forEach(imagem => {
 
     imagem.addEventListener('mouseout', () => {
         divTexto.style.opacity = '0';
+    });
+});
+
+
+const imagens2 = document.querySelectorAll('.tecnologias');
+const divTexto2 = document.querySelector('.div-texto2');
+
+const texto2 = {
+    'imagem-1': 'Html<br>Intermediário',
+    'imagem-2': 'CSS<br>Intermédiario',
+    'imagem-3': 'JavaScript<br>Iniciante',
+    'imagem-4': 'GitHub<br>Intermédiario',
+    'imagem-5': 'Vtex<br>Intermédiario',
+    'imagem-6': 'Sass<br>Intermédiario',
+    'imagem-7': 'React<br>Iniciante',
+    'imagem-8': 'Git<br>Intermédiario',
+};
+
+imagens2.forEach(imagems => {
+    const id = imagems.id;
+    const textoImagem2 = texto2[id];
+
+    imagems.addEventListener('mouseover', () => {
+        divTexto2.innerHTML = textoImagem2;
+        divTexto2.style.opacity = '1';
+    });
+
+    imagems.addEventListener('mouseout', () => {
+        divTexto2.style.opacity = '0';
     });
 });
