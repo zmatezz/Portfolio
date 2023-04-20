4a2ccb3865a75ff77b474e9c888b1ad9b313754e
Matheus - Aula 1 <matheusguedescosta@gmail.com>
2023-04-20 02:03:30 -0300

    rolagem suave

--- assets/js/scroll-reveal.js
+++ assets/js/scroll-reveal.js
@@ -10,3 +10,18 @@ sr.reveal('.escrita-projetos', { distance: "40px", duration: 2100, origin: 'top'
 sr.reveal('.terceiro-container', { distance: "40px", duration: 2500, origin: 'bottom' })
 sr.reveal('.escrita-habilidades', { distance: "60px", duration: 2500, origin: 'right' })
 sr.reveal('.container-tecnologias', { distance: "60px", duration: 2500, origin: 'left' })
+
+/*rolagem suave menu*/
+const linksMenu = document.querySelectorAll('.menu a');
+
+linksMenu.forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const section = document.querySelector(this.getAttribute('href'));
+
+    section.scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
