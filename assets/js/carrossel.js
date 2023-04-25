2f443baf169b5610408b9d7983b429fce5b93079
Matheus - Aula 1 <matheusguedescosta@gmail.com>
2023-04-25 01:49:48 -0300

    att carrosel

--- assets/js/carrossel.js
+++ assets/js/carrossel.js
@@ -1,18 +1,16 @@
 const radios = document.querySelectorAll('input[type="radio"]');
 let currentRadio = 0;
-
+let autoplayIntervalId = setInterval(autoplay, 3000);
 
 function autoplay() {
   currentRadio = currentRadio < radios.length - 1 ? currentRadio + 1 : 0;
   radios[currentRadio].click();
 }
 
-const autoplayInterval = 3000;
-setInterval(autoplay, autoplayInterval);
-
 radios.forEach((radio, index) => {
   radio.addEventListener('click', () => {
+    clearInterval(autoplayIntervalId);
+    autoplayIntervalId = setInterval(autoplay, 3000);
     currentRadio = index;
   });
 });
-
