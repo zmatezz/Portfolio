76c2b4ac1c1a6dc2ff2d073d55946e4016fd34d7
Matheus - Aula 1 <matheusguedescosta@gmail.com>
2023-05-04 13:19:10 -0300

    teste-digitando

--- assets/js/digitando-nome.js
+++ assets/js/digitando-nome.js
@@ -1,14 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const desktopVideo = document.querySelector('.video-background video', '.video-background-mobile video');
+  var meuVideo = document.getElementById('meu-video-desktop') || document.getElementById('meu-video-mobile');
 
-  function activateScript() {
+  meuVideo.addEventListener('loadeddata', function () {
     var typed = new Typed(".texto-escrevendo", {
       strings: ["Matheus Guedes", "Dev Front End"],
+
       backDelay: 1800,
       typeSpeed: 90,
       backSpeed: 80,
       loop: true,
-      startDelay: 3000,
+      startDelay: 3500,
       onComplete: function (self) {
         self.startDelay = 0;
       },
@@ -16,10 +17,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
     setTimeout(function () {
       $('.ocultar').fadeIn();
-    }, 3000);
-  }
-
-  desktopVideo.addEventListener('loadedmetadata', function () {
-    activateScript();
+    }, 3500);
   });
 });
