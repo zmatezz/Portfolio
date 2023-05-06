0588821be5019e12a3e35d6118e2677c9c61448f
Matheus - Aula 1 <matheusguedescosta@gmail.com>
2023-05-06 08:24:30 -0300

    hotfix onload

--- assets/js/digitando-nome.js
+++ assets/js/digitando-nome.js
@@ -1,10 +1,9 @@
-document.addEventListener('DOMContentLoaded', function () {
+window.onload = function () {
   var meuVideo = document.getElementById('meu-video-desktop') || document.getElementById('meu-video-mobile');
 
   meuVideo.addEventListener('loadeddata', function () {
     var typed = new Typed(".texto-escrevendo", {
       strings: ["Matheus Guedes", "Dev Front End"],
-
       backDelay: 1800,
       typeSpeed: 90,
       backSpeed: 80,
@@ -14,24 +13,17 @@ document.addEventListener('DOMContentLoaded', function () {
         self.startDelay = 0;
       },
     });
-
-
   });
-
-  window.onload = function () {
-    setTimeout(function () {
-      document.querySelector('.barra').classList.add('mostrar');
-    }, 3500);
-
-    setTimeout(function () {
-      document.querySelector('.box').classList.add('aparece');
-    }, 3500);
-  };
 
   setTimeout(function () {
     $('.ocultar').fadeIn();
-  }, 3500);
+  }, 3000);
 
+  setTimeout(function () {
+    document.querySelector('.barra').classList.add('mostrar');
+  }, 3000);
 
-
-});
+  setTimeout(function () {
+    document.querySelector('.box').classList.add('aparece');
+  }, 3000);
+};
