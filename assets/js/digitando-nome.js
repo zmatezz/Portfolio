c5a68d7685c57eb52380e96cb3b0ba51436d9a2e
Matheus - Aula 1 <matheusguedescosta@gmail.com>
2023-05-07 22:19:00 -0300

    hotfix mobile3-teste

--- assets/js/digitando-nome.js
+++ assets/js/digitando-nome.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', function () {
+window.onload = function () {
   var meuVideo = document.getElementById('meu-video-desktop') || document.getElementById('meu-video-mobile');
 
   meuVideo.addEventListener('canplaythrough', function () {
@@ -30,4 +30,4 @@ window.addEventListener('load', function () {
   setTimeout(function () {
     document.querySelector('.close-menu-label').classList.add('exibe');
   }, 2200);
-});
+};
