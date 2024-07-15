b3b7208cc619fa0a8b7e76867c061c6b96e911e2
zmatezz <matheusguedescosta@gmail.com>
2024-07-14 22:29:25 -0300

    feat: home knowledge section

---
+++ app/components/pages/home/known-techs/index.tsx
@@ -0,0 +1,25 @@
+import { SectionTitle } from "@/app/components/section-title";
+import { KnowTech } from "./know-tech";
+import { TbBrandNextjs } from "react-icons/tb";
+
+export const KnowTechs = () => {
+  return (
+    <section className="container py-16">
+      <SectionTitle subtitle="competências" title="Conhecimentos" />
+      <div className="grid grid-cols-[repeat(auto-fit,minmax(264px,1fr))] gap-3 mt-[60px]">
+        {Array.from({
+          length: 8,
+        }).map((_, index) => (
+          <KnowTech
+            key={index}
+            tech={{
+              icon: <TbBrandNextjs />,
+              name: "Next.js",
+              startDate: "2023-07-01",
+            }}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
